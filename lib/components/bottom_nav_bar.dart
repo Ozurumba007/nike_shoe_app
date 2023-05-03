4422a886fab4ae8c56efe011ae26805cb4da31b4
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-03 22:20:21 +0100

    added a setstate to the nav bar for navigating \ added an onTapChange to the bottom nav bar file \ created a list of pages in the homescreen.dart file

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({Key? key}) : super(key: key);
+  void Function(int)? onTapChange;
+  MyBottomNavBar({super.key, required this.onTapChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
       child: GNav(
-        tabs: [
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 26,
+        onTabChange: (value) => onTapChange!(value),
+        tabs: const [
           GButton(
             icon: Icons.home,
             text: 'Shop',
           ),
           GButton(
-            icon: Icons.home,
+            icon: Icons.shopping_bag,
             text: 'Cart',
           ),
         ],
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_store/components/bottom_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +9,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // this selected index is to control the bottom nav bar
+
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // screens to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+    // cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar:
+          MyBottomNavBar(onTapChange: (index) => navigateBottomBar(index)),
+    );
   }
 }
