e1f32c07766b7c6c39055c9f1b4ae4a2586c847e
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-03 21:09:45 +0100

    added a title and subtitle to the intro screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -7,26 +7,50 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          // Logo
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              // Logo
 
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Image.asset(
-              'lib/images/nike_logo.png',
-              height: 200,
-            ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/nike_logo.png',
+                  height: 200,
+                ),
+              ),
+
+              const SizedBox(height: 48),
+
+              // title
+
+              Text(
+                'Just Do It',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+
+              const SizedBox(height: 48),
+
+              // sub title
+
+              Text(
+                'Brand new sneakers and custom kicks made with premuim quality',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              // start new button
+            ],
           ),
-
-          const SizedBox(height: 48),
-
-          // title
-
-          // sub title
-
-          // start new button
-        ],
+        ),
       ),
     );
   }
