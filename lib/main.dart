c6ebcd98ece9f4ead0d0fd9a827ec3b6c0b4c4ee
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-03 20:24:45 +0100

    created a screen folder \ created an intro screen in the folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/intro_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  ,
+      home: IntroScreen(),
     );
   }
 }
