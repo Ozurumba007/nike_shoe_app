405710f03c1bfda7e1a2cf344cd0c498f272f310
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-05 01:18:23 +0100

    created a new file called cart_item.dart in the component folder

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
+import '../models/shoe.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Cart'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            Text(
+              'My Cart',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  // get individual shoe
+                  Shoe individualShoe = value.getUserCart()[index];
+                  // return cart item
+                  return CartItem();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
